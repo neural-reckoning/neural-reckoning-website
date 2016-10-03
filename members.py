1b143d31c3beb928c31bb20dafda95e69255031b
thesamovar <dg.github@thesamovar.net>
2016-10-03 16:25:33 +0100

    Added new PhD students

--- members.py
+++ members.py
@@ -56,6 +56,29 @@ members = [
         </p>
         ''',
         ),
+    ############# PHD STUDENTS ################################
+    # Alphabetical order
+    Member(
+        id='pamela_hathway',
+        name='Pamela Hathway',
+        author_names=['Hathway P'],
+        position='3_phd',
+        associate=False,
+        show_publications=True,
+        # generate this by import base64; base64.b64encode('address')
+        email='cC5oYXRod2F5MTZAaW1wZXJpYWwuYWMudWs=',
+        short='''
+        Auditory neuroscience: hearing in difficult environments
+        ''',
+        long='''
+        <p>
+        Pamela Hathway is a PhD student working on the problem of hearing
+        in difficult auditory environments (such as noisy restaurants). She is
+        interested in understanding how the brain copes with these problems,
+        and whether or not we can help using neural prostheses.
+        </p>
+        ''',
+        ),
     Member(
         id='jean_hugues_lestang',
         name='Jean-Hugues Lestang',
@@ -76,5 +99,71 @@ members = [
         <p>His Imperial webpage is <a href="http://www.ee.ic.ac.uk/electricalengineering/eepeople/person.asp?f=rg&c=ISN&s=NIL&id=30033">here</a>.
         ''',
         ),
+    Member(
+        id='lotte_weerts',
+        name='Lotte Weerts',
+        author_names=['Weerts L'],
+        position='3_phd',
+        associate=False,
+        show_publications=True,
+        # generate this by import base64; base64.b64encode('address')
+        email='bG90dGUud2VlcnRzMTVAaW1wZXJpYWwuYWMudWs=',
+        short='''
+        Auditory neuroscience: plasticity across multiple timescales
+        ''',
+        long='''
+        <p>
+        Lotte Weerts is a PhD student in the
+        <a href="http://www.imperial.ac.uk/neurotechnology/cdt/">Neurotechnology CDT</a>,
+        working on how the brain learns to cope with different auditory
+        environments. She is jointly supervised by
+        <a href="http://www.bg.ic.ac.uk/research/c.clopath/members/claudia_clopath/">Claudia Clopath</a>.
+        </p>
+        ''',
+        ),
+    Member(
+        id='chu_yang',
+        name='Chu Yang',
+        author_names=['Chu Y'],
+        position='3_phd',
+        associate=False,
+        show_publications=True,
+        # generate this by import base64; base64.b64encode('address')
+        #email='',
+        short='''
+        Neural network simulation techniques
+        ''',
+        long='''
+        <p>
+        Chu Yang is a PhD student, working on high performance spiking neural
+        network simulation, particularly in non-standard computational
+        hardware such as FPGAs. He is jointly supervised by
+        <a href="http://www.imperial.ac.uk/people/w.luk">Wayne Luk</a>.
+        </p>
+        ''',
+        ),
+    Member(
+        id='jonathan_zheng',
+        name='Jonathan Zheng',
+        author_names=['Zheng J'],
+        position='3_phd',
+        associate=False,
+        show_publications=True,
+        # generate this by import base64; base64.b64encode('address')
+        email='am9uYXRoYW4uemhlbmcxMkBpbXBlcmlhbC5hYy51aw==',
+        short='''
+        Interactive systems for ecosystems and neuroscience
+        ''',
+        long='''
+        <p>
+        Jonathan Zheng is a PhD student, working on interactive systems for
+        visualising the behaviour of ecosystems and neural networks. He is
+        jointly supervised by
+        <a href="http://www.imperial.ac.uk/people/s.pawar">Samraat Pawar</a>.
+        You can see an early prototype of our work in ecosystems at the
+        <a href="http://ecobuildergame.org/">EcoBuilder</a> website.
+        </p>
+        ''',
+        ),
 
-    ]
+]
